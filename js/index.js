3f5ead352a7f7cb4162265474da0b59ac328e429
Mianto <siddhantshaw97@gmail.com>
2018-12-03 01:06:23 +0530

    arrow for text added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 
 let infopop = document.getElementById("infopop");
 let sworkspop = document.getElementById("sworkspop");
+let horizontalScroll = document.getElementsByClassName("horizontal-scroll");
+let upButton = document.getElementsByClassName("upButton");
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
@@ -25,3 +27,34 @@ function openSworks() {
         sworkspop.style.display = "none"
     }
 }
+
+function showText(i) {
+    console.log(upButton[i].parentNode.children[1].style.display);
+    if (upButton[i].parentNode.children[1].style.display == "none" || upButton[i].parentNode.children[1].style.display == "") {
+        upButton[i].parentNode.children[1].style.display = "block";
+        upButton[i].innerHTML = "▼";
+        upButton[i].style.background = "rgba(10, 10, 10, 0.1)";
+    } else {
+        upButton[i].parentNode.children[1].style.display = "none";
+        upButton[i].innerHTML = "▲";
+        upButton[i].style.background = "rgba(220, 220, 220, 0.4)";
+    }
+}
+
+
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+            horizontalScroll.scrollLeft -= 100;
+            break;
+        case 38:
+            scrollBy(-50, 0);
+            break;
+        case 39:
+            horizontalScroll.scrollTop += 100;
+            break;
+        case 40:
+            scrollBy(50, 0);
+            break;
+    }
+};
