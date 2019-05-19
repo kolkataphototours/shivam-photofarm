18ec0cced46d4eab3ca50c47d3ed44a6a947d972
mianto <siddhantshaw97@gmail.com>
2019-05-19 18:40:49 +0530

    id changed from numbers to string

--- js/index.js
+++ js/index.js
@@ -57,3 +57,14 @@ document.onkeydown = function(e) {
             break;
     }
 };
+
+window.addEventListener("load",function() {
+    setTimeout(function(){
+      window.scrollTo(0, 10);
+    }, 1);
+    setTimeout(function(){
+      window.scrollTo(0, 10);
+    }, 0
+   );
+  });
+  
